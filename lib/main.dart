2f585c99b41ebea27fb4b880ddc3893a3e73e1ca
d4kash <medakshjha@gmail.com>
2023-05-09 08:25:00 +0530

    medicare Pushed

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:doc_trial/routes/router.dart';
+import 'package:doc_trial/utils/textscale.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: fixTextScale,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: routes,
+    );
+  }
+}
---
+++ lib/tabs/HomeTab.dart
@@ -0,0 +1,482 @@
+import 'package:doc_trial/styles/colors.dart';
+import 'package:doc_trial/styles/styles.dart';
+import 'package:flutter/material.dart';
+
+List<Map> doctors = [
+  {
+    'img': 'assets/doctor02.png',
+    'doctorName': 'Dr. Sahil',
+    'doctorTitle': 'Heart Specialist'
+  },
+  {
+    'img': 'assets/doctor03.jpeg',
+    'doctorName': 'Dr. Subramanyam',
+    'doctorTitle': 'Skin Specialist'
+  },
+  {
+    'img': 'assets/doctor02.png',
+    'doctorName': 'Dr. Sunaina Singh',
+    'doctorTitle': 'Heart Specialist'
+  },
+  {
+    'img': 'assets/doctor03.jpeg',
+    'doctorName': 'Dr. Tarun Gill',
+    'doctorTitle': 'Skin Specialist'
+  }
+];
+
+class HomeTab extends StatelessWidget {
+  final void Function() onPressedScheduleCard;
+
+  const HomeTab({
+    Key? key,
+    required this.onPressedScheduleCard,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const UserIntro(),
+            const SizedBox(
+              height: 10,
+            ),
+            const SearchInput(),
+            const SizedBox(
+              height: 20,
+            ),
+            const CategoryIcons(),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Appointment Today',
+                  style: kTitleStyle,
+                ),
+                TextButton(
+                  child: Text(
+                    'See All',
+                    style: TextStyle(
+                      color: Color(MyColors.yellow01),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            AppointmentCard(
+              onTap: onPressedScheduleCard,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Top Doctor',
+              style: TextStyle(
+                color: Color(MyColors.header01),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            for (var doctor in doctors)
+              TopDoctorCard(
+                img: doctor['img'],
+                doctorName: doctor['doctorName'],
+                doctorTitle: doctor['doctorTitle'],
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TopDoctorCard extends StatelessWidget {
+  String img;
+  String doctorName;
+  String doctorTitle;
+
+  TopDoctorCard({
+    required this.img,
+    required this.doctorName,
+    required this.doctorTitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, '/detail');
+        },
+        child: Row(
+          children: [
+            Container(
+              color: Color(MyColors.grey01),
+              child: Image(
+                width: 100,
+                image: AssetImage(img),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  doctorName,
+                  style: TextStyle(
+                    color: Color(MyColors.header01),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  doctorTitle,
+                  style: TextStyle(
+                    color: Color(MyColors.grey02),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Color(MyColors.yellow02),
+                      size: 18,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '4.0 - 50 Reviews',
+                      style: TextStyle(color: Color(MyColors.grey02)),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppointmentCard extends StatelessWidget {
+  final void Function() onTap;
+
+  const AppointmentCard({
+    Key? key,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Color(MyColors.primary),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: onTap,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          backgroundImage: AssetImage('assets/doctor01.jpeg'),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text('Dr. Sahil',
+                                style: const TextStyle(color: Colors.white)),
+                            const SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              'Dental Specialist',
+                              style: TextStyle(color: Color(MyColors.text01)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const ScheduleCard(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          width: double.infinity,
+          height: 10,
+          decoration: BoxDecoration(
+            color: Color(MyColors.bg02),
+            borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 40),
+          width: double.infinity,
+          height: 10,
+          decoration: BoxDecoration(
+            color: Color(MyColors.bg03),
+            borderRadius: const BorderRadius.only(
+              bottomRight: const Radius.circular(10),
+              bottomLeft: const Radius.circular(10),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+List<Map> categories = [
+  {'icon': Icons.coronavirus, 'text': 'Covid 19'},
+  {'icon': Icons.local_hospital, 'text': 'Hospital'},
+  {'icon': Icons.car_rental, 'text': 'Ambulance'},
+  {'icon': Icons.local_pharmacy, 'text': 'Pill'},
+];
+
+class CategoryIcons extends StatelessWidget {
+  const CategoryIcons({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        for (var category in categories)
+          CategoryIcon(
+            icon: category['icon'],
+            text: category['text'],
+          ),
+      ],
+    );
+  }
+}
+
+class ScheduleCard extends StatelessWidget {
+  const ScheduleCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(MyColors.bg01),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Icon(
+            Icons.calendar_today,
+            color: Colors.white,
+            size: 15,
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Text(
+            'Mon, July 29',
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Icon(
+            Icons.access_alarm,
+            color: Colors.white,
+            size: 17,
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Flexible(
+            child: Text(
+              '11:00 ~ 12:10',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryIcon extends StatelessWidget {
+  IconData icon;
+  String text;
+
+  CategoryIcon({
+    required this.icon,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Color(MyColors.bg01),
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Column(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(MyColors.bg),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Icon(
+                icon,
+                color: Color(MyColors.primary),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                color: Color(MyColors.primary),
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchInput extends StatelessWidget {
+  const SearchInput({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(MyColors.bg),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 3),
+            child: Icon(
+              Icons.search,
+              color: Color(MyColors.purple02),
+            ),
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search a doctor or health issue',
+                hintStyle: TextStyle(
+                    fontSize: 13,
+                    color: Color(MyColors.purple01),
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UserIntro extends StatelessWidget {
+  const UserIntro({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Hello',
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            Text(
+              'Paitent name 👋',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ],
+        ),
+        const CircleAvatar(
+          backgroundImage: AssetImage('assets/person.jpeg'),
+        )
+      ],
+    );
+  }
+}
